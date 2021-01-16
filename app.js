84c8b54452e537fb09c054e5186e2161d17acef4
Franklin Noriega <zerocreative.f@gmail.com>
2021-01-15 23:58:02 -0400

    twitter beermoney bot DONE

--- app.js
+++ app.js
@@ -1,20 +1,55 @@
 import config from 'config';
-import { connection } from './db.mjs';
+import { connection, getNewlyCreatedFloors, updateTweetFloor, getInitialFloor } from './db.mjs';
 import { twitterClient } from './api.mjs';
 
-const twitter = twitterClient(config.keys);
-let data = [];
-
-// console.log('Tweet');
-// data = await twitterClient.tweets.statusesUpdate({ status: 'Respondiendo al Hola Mundo desde el API', in_reply_to_status_id: '1349951366161199104' });
-
-console.log('Timeline');
-data = await twitter.tweets.statusesUserTimeline({ screen_name: 'Beermoney_Bot' });
-
-// console.log('Get By ID');
-// data = await twitterClient.tweets.statusesShowById({ id: '1349951366161199104' });
-
-for (let tweet of data) {
-  console.log('get tweet!!!');
-  console.log(tweet.text);
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
 }
+
+(async function () {
+  const twitter = twitterClient(config.keys);
+  let dbConnection = await connection(config.DB);
+  let floor = [];
+
+  while (1 == 1) {
+    floor = await getNewlyCreatedFloors(dbConnection);
+    if (floor.length) {
+      floor = floor[0];
+
+      let tweet = '';
+      let status = {};
+
+      // ENTRADA
+      if (floor.Level == 0) {
+        tweet += 'Par: $' + floor.Asset + ' / #BTC\n';
+        tweet += 'Precio de Entrada: ' + floor.Price + ' sats\n\n';
+        tweet += '#AlgorithmicTrading #TradingPlan' + floor.FK_Trading_Plan + ' 🍺';
+        status = await twitter.tweets.statusesUpdate({ status: tweet });
+        //SALIDA
+      } else {
+        tweet += 'Par: $' + floor.Asset + ' / #BTC\n';
+        tweet += 'Precio de Salida: ' + floor.Price + ' sats\n';
+
+        if (floor.Profit > 0) {
+          tweet += 'Ganancia: ' + floor.Profit + '%\n\n';
+          tweet += '#AlgorithmicTrading #TradingPlan' + floor.FK_Trading_Plan;
+          tweet += '😎🍻\n\n';
+          tweet += 'https://www.binance.com/es/trade/' + floor.Asset + '_BTC';
+        } else {
+          tweet += 'Perdida: ' + floor.Profit + '%\n\n';
+          tweet += '#AlgorithmicTrading #TradingPlan' + floor.FK_Trading_Plan;
+          tweet += '😥\n\n';
+          tweet += 'https://www.binance.com/es/trade/' + floor.Asset + '_BTC';
+        }
+
+        let initialFloor = await getInitialFloor(dbConnection, floor.FK_Trading_Plan);
+        status = await twitter.tweets.statusesUpdate({ status: tweet, in_reply_to_status_id: initialFloor.TweetID });
+      }
+
+      updateTweetFloor(dbConnection, floor.ID, status.id_str);
+    }
+    await sleep(1000);
+  }
+})();
--- db.mjs
+++ db.mjs
@@ -1,8 +1,10 @@
 import mysql from 'promise-mysql';
 
+export let db_config = {};
+
 // Connect to the DB and get the Pool of it to perform querys
 export function connection(database) {
-  const db_config = {
+  db_config = {
     host: database.Host,
     user: database.User,
     password: database.Password,
@@ -11,4 +13,41 @@ export function connection(database) {
   };
 
   return mysql.createPool(db_config);
+}
+
+export async function getNewlyCreatedFloors(dbConnection) {
+  try {
+    let timeCondition = '(UNIX_TIMESTAMP(CURRENT_TIME())-UNIX_TIMESTAMP(f.DateTime))/60 <1440'; //1 Minute old
+
+    let result = await dbConnection.query('SELECT * FROM floor f WHERE (Level=0 OR Level=-2) AND TweetID is Null AND OrderID>0 AND ' + timeCondition);
+    return result;
+  } catch (e) {
+    console.log(e);
+    console.log('getNewlyCreatedFloor Error');
+    return false;
+  }
+}
+
+export async function updateTweetFloor(dbConnection, id, tweetID) {
+  try {
+    let result = await dbConnection.query('UPDATE floor SET TweetID=' + tweetID + '  WHERE ID=' + id);
+
+    return result;
+  } catch (e) {
+    console.log(e);
+    console.log('updateTweetFloor Error');
+    return false;
+  }
+}
+
+export async function getInitialFloor(dbConnection, tradingPlanID) {
+  try {
+    let result = await dbConnection.query('SELECT * FROM floor f WHERE Level=0 AND FK_Trading_Plan=' + tradingPlanID);
+
+    return result[0];
+  } catch (e) {
+    console.log(e);
+    console.log('getInitialFloor Error');
+    return false;
+  }
 }
